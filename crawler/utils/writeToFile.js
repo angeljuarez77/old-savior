e47e2c01c2e91016ea07d05ce2589343cc5de23e
Angel Juarez <angeljuarez729@gmail.com>
2023-02-11 00:57:54 -0500

    extending functionality

--- crawler/utils/writeToFile.js
+++ crawler/utils/writeToFile.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-const writeToFile = (content, filename) => {
-  const imagePath = path.join(__dirname, '..', '..', 'images', `${filename}.jpg`);
+const writeToFile = (content, filename, extension) => {
+  const imagePath = path.join(__dirname, '..', '..', 'images', `${filename}.${extension}`);
   fs.writeFile(imagePath, content, (e) => {
     if(e) {
       console.error(e.message);
     }
-    console.log('File written!');
   });
 }
 
