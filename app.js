b1745e0b9e4792f44b1ffb8b371b9910517f975a
Angel Juarez <angeljuarez729@gmail.com>
2023-02-14 00:15:37 -0500

    jwt implemented

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { crawler } = require('./crawler/puppeteer');
+const { auth } = require('express-oauth2-jwt-bearer');
+
+const jwtCheck = auth({
+  audience: 'tools.angeldevelops.com',
+  issuerBaseURL: 'https://dev-yoyovc4d.us.auth0.com/',
+  tokenSigningAlg: 'RS256'
+});
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(jwtCheck)
 
 app.post('/save', async (req, res) => {
   const { body: { url } } = req;
@@ -15,7 +23,7 @@ app.post('/save', async (req, res) => {
     console.error(e.message);
     res.json({ message: e.message });
   }
-})
+});
 
 module.exports = {
   app
